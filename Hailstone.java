d084d98250d9077a7852e0035a88ebba628a6fda
RM <robsm2@hotmail.com>
2016-10-31 21:37:40 -0700

    in the process of making my sequence program flexible for any number of operations

--- Hailstone.java
+++ Hailstone.java
@@ -5,26 +5,41 @@ public class Hailstone{
 		Scanner s1 = new Scanner(System.in);
 	//initialize and inform user of program function
 	System.out.println("This program performs operations on a given integer to create a hailstone sequence of given length. The hailstone sequence is formed by operations: (if odd (3x+1) if even (x/2). The purpose is to find if all hailstone sequences end the same way.");
-	System.out.println("please enter an integer for the sequence:");
+	System.out.println("please enter a starting integer for the sequence:");
 	long num = s1.nextLong();
-	System.out.println("please enter an integer to denote number of times oepration is performed (n):");
+	System.out.println("please enter a value n where n = number of times operation is performed:");
 	long n = s1.nextLong();
+	System.out.println("please enter the number of different operations defining the sequence. e.g. 2 if there is one operattion for odd numbers, and 1 operation for even numbers:");
+	int numOperations = s1.nextInt();
+	String[] operations = new String[numOperations];
+	for(int i = 0; i < numOperations; i++){
+		System.out.println("please enter formula" + i);
+		operations[i] = s1.next();
+	}
+
+	System.out.println("please enter corresponding conditions (in java friendly format) for respective operations implementation. e.g. \"x%2 == 0\" for condition x is even");
+	String[] conditions = new String[numOperations];
+        for(int i = 0; i < numOperations; i++){
+                System.out.println("please enter formula" + i);
+                conditions[i] = s1.next();
+        }
+
 	System.out.println("");
-	//System.out.Println("please enter desired operation for odd integers");
-	//String odd = s1.next();
-	//System.out.Println("please enter desired operation for odd integers");
-	//String even = s1.next();
-	System.out.print("Hailstone sequence = " + num + ",");	
+
+	System.out.print("sequence = " + num + ",");
+	int count = 0; //keeps count of operations and conditions arrays	
 	for(int i = 0; i < n; i++){
-		num = function(num);
-		System.out.print(num + ",");	
+		num = function(num,operations,conditions,count);
+		System.out.print(num + ",");
+		count++;	
 	}
 	System.out.println("");
 	return;
 }
 
 	//static method performs the math equation
-	private static long function(long num){
+	private static long function(long num, String[] operations, String[] conditions, int count){
+		for(numOperations)
 		if(num%2 == 1){
 			num = num*3+1;
 			return num;
